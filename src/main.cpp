6b8e77cda544a50a785bc8ed76c8b5269fafe566
LUIDWIGI <empireprojectaltijd@gmail.com>
2023-06-23 19:02:42 +0200

    Added screen support + general optimizations
    Added:
    - Screen rendering
    
    Changed:
    - Optimized if statements with likely
    
    Fixed:
    - Actually defined pin modes (5Head)
    - Sussed the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include "RF24.h"			// Comm module library
-
+#include "oled.h"			// Screen library
 typedef uint8_t pin;			// Define pin data type as byte for better readability
 
 // Pins
@@ -8,6 +8,7 @@ const pin speedJoy = A1;	// Pin of the joystick that controls speed
 const pin steeringJoy = A2;	// Pin of the joystick that controls the steering
 const pin forward = 2;		// Pin that reads if the switch is in the forward position
 const pin backward = 3;		// Pin that reads if the switch is in the backward position
+
 
 // Structs that holds the data of the single package that the controller sends
 struct Package {
@@ -24,10 +25,32 @@ uint8_t pynqReading;				// Byte that holds the (RAW!!!!!) speed reading from the
 RF24 radio(9, 10);  // using pin 9 for the CE pin, and pin 10 for the CSN pin
 const uint8_t addresses[][6] = {"1Node", "2Node"};	// Define addresses as a byte
 
+// Create display object
+OLED display(18,
+			 19,
+			 NO_RESET_PIN,
+			 OLED::W_128,
+			 OLED::H_64,
+			 OLED::CTRL_SSD1306,
+			 0x3C);
+String displayData;
+
+uint8_t lastPynqReading;
+
 void setup() {
 // Open transmission pipes
 	radio.openWritingPipe(addresses[1]);
 	radio.openReadingPipe(1,addresses[0]);
+
+// Set up pins
+	pinMode(speedJoy, INPUT);
+	pinMode(steeringJoy, INPUT);
+	pinMode(forward, INPUT);
+	pinMode(backward, INPUT);
+
+	// Initialize display
+	display.begin();
+
 }
 
 void loop() {
@@ -53,5 +76,10 @@ radio.write(&package, sizeof(package));
 radio.startListening();
 if (radio.available()) {
 	radio.read(&pynqReading, sizeof(pynqReading));
+	if (pynqReading != lastPynqReading) [[unlikely]] {
+		display.draw_string(6, 8, displayData.c_str());
+		display.display();
+		lastPynqReading = pynqReading;
+	}
 }
 }
